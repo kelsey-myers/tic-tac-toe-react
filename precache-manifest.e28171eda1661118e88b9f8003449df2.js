746023661c1d8daa595dfee6ef4815c45b8263e3
Kelsey Myers <52179263+kelsey-myers@users.noreply.github.com>
2020-06-29 21:20:00 +0100

    Updates

---
+++ precache-manifest.e28171eda1661118e88b9f8003449df2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23139c54fa7546e9a44a",
+    "url": "/tic-tac-toe-react/static/css/main.2ca89015.chunk.css"
+  },
+  {
+    "revision": "23139c54fa7546e9a44a",
+    "url": "/tic-tac-toe-react/static/js/main.5d0f86ef.chunk.js"
+  },
+  {
+    "revision": "c650f4249a1a9ac15f21",
+    "url": "/tic-tac-toe-react/static/js/runtime~main.27a0c5ca.js"
+  },
+  {
+    "revision": "d696d576b8a1b596ce70",
+    "url": "/tic-tac-toe-react/static/js/2.1787dd6f.chunk.js"
+  },
+  {
+    "revision": "c12534bde21cefacef58c44c88209c6d",
+    "url": "/tic-tac-toe-react/index.html"
+  }
+];
